16d1a63c24cfb5a949cecabc1d65440522db296d
SaloAlex <dvdsalomon6@gmail.com>
2024-03-29 15:11:49 -0300

    server

--- server.js
+++ server.js
@@ -20,13 +20,13 @@ app.use(
 app.use(express.static(path.join(__dirname, "build")));
 
 const apiKey = "RGAPI-ac16c292-d2bb-45bd-8bef-6248f45782eb";
-const baseURL = "https://master--conect2.netlify.app";
+// const baseURL = "https://master--conect2.netlify.app";
 
 // Ruta para manejar la solicitud de búsqueda de jugadores de Riot Games por gameName y tagLine
 app.get("/riot/account/v1/accounts/by-riot-id/:gameName/:tagLine", async (req, res) => {
   try {
     const { gameName, tagLine } = req.params;
-    const response = await axios.get(`${baseURL}/riot/account/v1/accounts/by-riot-id/${encodeURIComponent(gameName)}/${encodeURIComponent(tagLine)}?api_key=${apiKey}`);
+    const response = await axios.get(`https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${encodeURIComponent(gameName)}/${encodeURIComponent(tagLine)}?api_key=${apiKey}`);
     res.json(response.data);
   } catch (error) {
     console.error("Error fetching player data:", error);
@@ -38,7 +38,7 @@ app.get("/riot/account/v1/accounts/by-riot-id/:gameName/:tagLine", async (req, r
 app.get("/lol/summoner/v4/summoners/by-name/:summonerName", async (req, res) => {
   try {
     const { summonerName } = req.params;
-    const response = await axios.get(`${baseURL}/lol/summoner/v4/summoners/by-name/${encodeURIComponent(summonerName)}?api_key=${apiKey}`);
+    const response = await axios.get(`https://la2.api.riotgames.com/lol/summoner/v4/summoners/by-name/${encodeURIComponent(summonerName)}?api_key=${apiKey}`);
     res.json(response.data);
   } catch (error) {
     console.error("Error fetching summoner data:", error);
@@ -50,7 +50,7 @@ app.get("/lol/summoner/v4/summoners/by-name/:summonerName", async (req, res) => 
 app.get("/lol/league/v4/entries/by-summoner/:encryptedSummonerId", async (req, res) => {
   try {
     const { encryptedSummonerId } = req.params;
-    const response = await axios.get(`${baseURL}/lol/league/v4/entries/by-summoner/${encodeURIComponent(encryptedSummonerId)}?api_key=${apiKey}`);
+    const response = await axios.get(`https://la2.api.riotgames.com/lol/league/v4/entries/by-summoner/${encodeURIComponent(encryptedSummonerId)}?api_key=${apiKey}`);
     res.json(response.data);
   } catch (error) {
     console.error("Error fetching league data:", error);
