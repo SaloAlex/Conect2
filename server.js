620b43f7e16a55fa7c2c0d3266b7ba51be3e4bb8
SaloAlex <dvdsalomon6@gmail.com>
2024-03-30 19:07:03 -0300

    sdsd

--- server.js
+++ server.js
@@ -1,75 +1,75 @@
-// import express from "express";
-// import cors from "cors";
-// import { fileURLToPath } from "url";
-// import path from "path";
+import express from "express";
+import cors from "cors";
+import { fileURLToPath } from "url";
+import path from "path";
 
-// const { default: axios } = await import('axios');
+const { default: axios } = await import('axios');
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// const app = express();
-// const PORT = process.env.PORT || 3001;
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-// app.use(
-//   cors({
-//     origin: "https://master--conect2.netlify.app/", // Permitir el acceso desde cualquier origen
-//   })
-// );
+app.use(
+  cors({
+    origin: "https://master--conect2.netlify.app/", // Permitir el acceso desde cualquier origen
+  })
+);
 
-// app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "build")));
 
-// const apiKey = "RGAPI-ac16c292-d2bb-45bd-8bef-6248f45782eb";
-// // const baseURL = "https://master--conect2.netlify.app";
+const apiKey = "RGAPI-ac16c292-d2bb-45bd-8bef-6248f45782eb";
+// const baseURL = "https://master--conect2.netlify.app";
 
-// // Ruta para manejar la solicitud de búsqueda de jugadores de Riot Games por gameName y tagLine
-// app.get("/riot/account/v1/accounts/by-riot-id/:gameName/:tagLine", async (req, res) => {
-//   try {
-//     const { gameName, tagLine } = req.params;
-//     const response = await axios.get(`https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${encodeURIComponent(gameName)}/${encodeURIComponent(tagLine)}?api_key=${apiKey}`);
-//     res.json(response.data);
-//   } catch (error) {
-//     console.error("Error fetching player data:", error);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
+// Ruta para manejar la solicitud de búsqueda de jugadores de Riot Games por gameName y tagLine
+app.get("/riot/account/v1/accounts/by-riot-id/:gameName/:tagLine", async (req, res) => {
+  try {
+    const { gameName, tagLine } = req.params;
+    const response = await axios.get(`https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${encodeURIComponent(gameName)}/${encodeURIComponent(tagLine)}?api_key=${apiKey}`);
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching player data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
-// // Ruta para manejar la solicitud de búsqueda de jugadores por summonerName
-// app.get("/lol/summoner/v4/summoners/by-name/:summonerName", async (req, res) => {
-//   try {
-//     const { summonerName } = req.params;
-//     const response = await axios.get(`https://la2.api.riotgames.com/lol/summoner/v4/summoners/by-name/${encodeURIComponent(summonerName)}?api_key=${apiKey}`);
-//     res.json(response.data);
-//   } catch (error) {
-//     console.error("Error fetching summoner data:", error);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
+// Ruta para manejar la solicitud de búsqueda de jugadores por summonerName
+app.get("/lol/summoner/v4/summoners/by-name/:summonerName", async (req, res) => {
+  try {
+    const { summonerName } = req.params;
+    const response = await axios.get(`https://la2.api.riotgames.com/lol/summoner/v4/summoners/by-name/${encodeURIComponent(summonerName)}?api_key=${apiKey}`);
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching summoner data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
-// // Ruta para manejar la solicitud de búsqueda de datos de la liga por summonerId
-// app.get("/lol/league/v4/entries/by-summoner/:encryptedSummonerId", async (req, res) => {
-//   try {
-//     const { encryptedSummonerId } = req.params;
-//     const response = await axios.get(`https://la2.api.riotgames.com/lol/league/v4/entries/by-summoner/${encodeURIComponent(encryptedSummonerId)}?api_key=${apiKey}`);
-//     res.json(response.data);
-//   } catch (error) {
-//     console.error("Error fetching league data:", error);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
+// Ruta para manejar la solicitud de búsqueda de datos de la liga por summonerId
+app.get("/lol/league/v4/entries/by-summoner/:encryptedSummonerId", async (req, res) => {
+  try {
+    const { encryptedSummonerId } = req.params;
+    const response = await axios.get(`https://la2.api.riotgames.com/lol/league/v4/entries/by-summoner/${encodeURIComponent(encryptedSummonerId)}?api_key=${apiKey}`);
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching league data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
-// // Ruta para manejar la solicitud de envío de correo electrónico
-// app.post("/send-email", async (req, res) => {
-//   try {
-//     // Lógica para enviar correo electrónico
-//     res.json({ success: true });
-//   } catch (error) {
-//     console.error("Error sending email:", error);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
+// Ruta para manejar la solicitud de envío de correo electrónico
+app.post("/send-email", async (req, res) => {
+  try {
+    // Lógica para enviar correo electrónico
+    res.json({ success: true });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
-// // Iniciar el servidor en el puerto especificado
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+// Iniciar el servidor en el puerto especificado
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- src/components/Riot.jsx
+++ src/components/Riot.jsx
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import axios from "axios";
 import LogoLol from "../assets/lol.png";
 
-
 const Riot = () => {
   const [gameName, setGameName] = useState("");
   const [tagLine, setTagLine] = useState("");
   const [playerData, setPlayerData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const handleLoginWithRiot = () => {
+    window.location.href = "https://auth.riotgames.com/authorize?client_id=670315&redirect_uri=http://localhost:5173/user-profile&response_type=code&scope=openid+offline_access";
+  };
 
   const handleGameNameChange = (event) => {
     setGameName(event.target.value);
@@ -18,41 +20,55 @@ const Riot = () => {
     setTagLine(event.target.value);
   };
 
-  const searchPlayer = async () => {
+  const handleSearch = async () => {
     setLoading(true);
     setError(null);
+
     try {
-      const riotAccountResponse = await axios.post("/api/searchPlayer", {
-        gameName,
-        tagLine,
+      // Endpoint 1: Buscar por gameName y tagLine
+      const riotAccountEndpoint =
+        "http://localhost:3001/riot/account/v1/accounts/by-riot-id";
+      const riotAccountResponse = await axios.get(riotAccountEndpoint, {
+        params: { gameName, tagLine },
       });
-  
-      const summonerResponse = await axios.post("/api/getSummoner", {
-        summonerName: gameName,
+
+      // Endpoint 2: Buscar por summonerName (usando el mismo gameName)
+      const summonerEndpoint =
+        "http://localhost:3001/lol/summoner/v4/summoners/by-name";
+      const summonerResponse = await axios.get(summonerEndpoint, {
+        params: { summonerName: gameName },
       });
-  
-      const leagueResponse = await axios.get(`/api/getLeague/${summonerResponse.data.id}`);
-  
+
+      // Endpoint 3: Buscar por encryptedSummonerId (usando el summonerId del segundo endpoint)
+      const leagueEndpoint = `http://localhost:3001/lol/league/v4/entries/by-summoner/${summonerResponse.data.id}`;
+      const leagueResponse = await axios.get(leagueEndpoint);
+
+      // Actualizar el estado con los datos de los tres endpoints
       setPlayerData({
         riotAccount: riotAccountResponse.data,
         summoner: summonerResponse.data,
         league: leagueResponse.data,
       });
     } catch (error) {
-      console.error("Error searching player:", error.response ? error.response.data : error.message);
+      console.error(
+        "Error searching player:",
+        error.response ? error.response.data : error.message
+      );
       setError("Error searching player. Please try again.");
     } finally {
       setLoading(false);
     }
   };
 
-  
-
   return (
     <div className="max-w-lg mx-auto mt-10 p-6 bg-white rounded-lg shadow-md">
       <h2 className="text-2xl mb-4 flex text-center">
-        Riot Games
-        <img src={LogoLol} alt="Riot Logo" className="ml-2 h-10 w-10" />
+        Connect{" "}
+        <img
+          src={LogoLol} // Reemplaza con la ruta correcta de tu imagen
+          alt="Riot Logo"
+          className="ml-2 h-10 w-10" // Puedes ajustar las clases según tus necesidades
+        />
       </h2>
 
       <div className="mb-4">
@@ -79,15 +95,21 @@ const Riot = () => {
           className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
         />
       </div>
-      <button
-        onClick={searchPlayer}
-        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-      >
-        {loading ? "Searching..." : "Connect"}
-      </button>
+      {/* Botón para iniciar sesión con la cuenta de Riot */}
+      <div className="text-center">
+        <a href="https://auth.riotgames.com/authorize?client_id=670315&redirect_uri=http://localhost:5173/user-profile&response_type=code&scope=openid+offline_access"
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Log in with Riot Account
+        </a>
+      </div>
+
       {error && <p className="mt-4 text-red-500">{error}</p>}
       {playerData && (
         <div className="mt-4">
+          {/* Utilizamos el profileIconId directamente desde la respuesta del segundo endpoint */}
           {playerData.summoner.profileIconId && (
             <img
               src={`https://opgg-static.akamaized.net/meta/images/profile_icons/profileIcon${playerData.summoner.profileIconId}.jpg?image=q_auto,f_webp,w_auto&v=1707283412529`}
@@ -100,6 +122,7 @@ const Riot = () => {
           <p>Name: {playerData.summoner.name}</p>
           <p>Summoner Level: {playerData.summoner.summonerLevel}</p>
 
+          {/* Información de la liga */}
           <h3 className="text-lg font-semibold">League:</h3>
           <p>Queue Type: {playerData.league[0].queueType}</p>
           <p>
@@ -113,9 +136,19 @@ const Riot = () => {
             )}
           </p>
           <p>Rank: {playerData.league[0].rank}</p>
-          <p>League Points: {playerData.league[0].leaguePoints}</p>
+          <p>LP: {playerData.league[0].leaguePoints}</p>
           <p>Wins: {playerData.league[0].wins}</p>
           <p>Losses: {playerData.league[0].losses}</p>
+
+              {/* Renderizar la lista de IDs de partidas */}
+    <div>
+      <h3 className="text-lg font-semibold">Match IDs:</h3>
+      <ul>
+        {playerData.matchIds.map((matchId, index) => (
+          <li key={index}>{matchId}</li>
+        ))}
+      </ul>
+    </div>
         </div>
       )}
     </div>
