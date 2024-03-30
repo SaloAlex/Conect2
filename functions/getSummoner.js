82be6ed93b7d548e48c6c72919baf89214ae85ab
SaloAlex <dvdsalomon6@gmail.com>
2024-03-30 15:35:15 -0300

    logica netlify

--- functions/getSummoner.js
+++ functions/getSummoner.js
@@ -1,13 +1,18 @@
-// getSummoner.js
-
 const axios = require('axios');
 
 exports.handler = async (event, context) => {
   try {
     const { summonerName } = JSON.parse(event.body);
     
+    // Tu clave de API de Riot Games
+    const apiKey = 'RGAPI-ac16c292-d2bb-45bd-8bef-6248f45782eb';
+    
     // Lógica para obtener información de un jugador por summonerName
-    const response = await axios.get(`https://la2.api.riotgames.com/lol/summoner/v4/summoners/by-name/${encodeURIComponent(summonerName)}`);
+    const response = await axios.get(`https://la2.api.riotgames.com/lol/summoner/v4/summoners/by-name/${encodeURIComponent(summonerName)}`, {
+      headers: {
+        'X-Riot-Token': apiKey
+      }
+    });
     
     return {
       statusCode: 200,
