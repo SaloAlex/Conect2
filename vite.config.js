2fbf4ae565d1d67f9a0277a76bb73adc622c02c8
SaloAlex <dvdsalomon6@gmail.com>
2024-03-30 17:07:28 -0300

    sdsd

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy:{
-      '/api': 'http://localhost:3001'
-       }
+    proxy: {
+      '/api': {
+        target: 'https://master--conect2.netlify.app/', // Reemplaza con la URL de tu sitio en Netlify
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
-      plugins: [react()]
-})
+    },
+  },
+});
